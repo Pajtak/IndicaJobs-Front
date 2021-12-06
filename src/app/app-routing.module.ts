87fc20745b4c0cd4c7f5097bcbb0f1a3e04b530a
Pajtak <brunofpajtak@gmail.com>
2021-12-05 22:33:52 -0300

    Elaboração da Página Inicial e Página Esqueceu a Senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { EsqueceuSenhaComponent } from './esqueceu-senha/esqueceu-senha.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo: 'bem-vindo', pathMatch: 'full'},
+
+  {path:'bem-vindo',component: BemVindoComponent},
+  {path:'login-cadastrar', component: LoginCadastroComponent},
+  {path:'esqueceu-senha', component: EsqueceuSenhaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginCadastroComponent } from './login-cadastro/login-cadastro.component';
-
+import { RodapeComponent } from './rodape/rodape.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { EsqueceuSenhaComponent } from './esqueceu-senha/esqueceu-senha.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginCadastroComponent,
 
+    LoginCadastroComponent,
+    RodapeComponent,
+    NavBarComponent,
+    BemVindoComponent,
+    EsqueceuSenhaComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
